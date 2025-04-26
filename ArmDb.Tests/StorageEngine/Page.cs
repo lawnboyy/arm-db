96b7f5c86320c252b2af573afa5dd0f9ed1a7bbb
Alan Hunt <alan.hunt@maplarge.com>
2025-04-26 10:03:08 -0500

    Add initial Page class

---
+++ ArmDb.Tests/StorageEngine/Page.cs
@@ -0,0 +1,11 @@
+namespace ArmDb.StorageEngine;
+
+/// <summary>
+/// Represents an in-memory copy of a fixed-size page from disk.
+/// Provides structured access to the underlying byte data.
+/// (Implementation details will be added incrementally)
+/// </summary>
+public sealed class Page
+{
+
+}
