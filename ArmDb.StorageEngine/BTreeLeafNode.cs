d505dd1d34fab133f5762864591b75bebb52faa6
Alan A. Hunt <a.a.hunt@gmail.com>
2025-09-21 13:17:09 -0500

    Create BTreeNode base class to share common state and method to find a key's slot index

--- ArmDb.StorageEngine/BTreeLeafNode.cs
+++ ArmDb.StorageEngine/BTreeLeafNode.cs
@@ -9,12 +9,8 @@ namespace ArmDb.StorageEngine;
 /// Represents a leaf node of a B+Tree structure. Leaf nodes are slotted pages that store
 /// the actual table records.
 /// </summary>
-internal sealed class BTreeLeafNode
+internal sealed class BTreeLeafNode : BTreeNode
 {
-  private readonly Page _page;
-  private readonly TableDefinition _tableDefinition;
-  private readonly KeyComparer _keyComparer = new KeyComparer();
-
   internal int ItemCount => new PageHeader(_page).ItemCount;
 
   internal int PageIndex => _page.Id.PageIndex;
@@ -45,23 +41,13 @@ internal sealed class BTreeLeafNode
     }
   }
 
-  internal BTreeLeafNode(Page page, TableDefinition tableDefinition)
+  internal BTreeLeafNode(Page page, TableDefinition tableDefinition) : base(page, tableDefinition)
   {
-    ArgumentNullException.ThrowIfNull(page);
-    ArgumentNullException.ThrowIfNull(tableDefinition);
-
     var header = SlottedPage.GetHeader(page);
-    if (header.PageType == PageType.Invalid)
-    {
-      throw new ArgumentException($"Received an invalid Page!", "page");
-    }
     if (header.PageType != PageType.LeafNode)
     {
       throw new ArgumentException($"Expected a leaf node page but recieved: {header.PageType}", "page");
     }
-
-    _page = page;
-    _tableDefinition = tableDefinition;
   }
 
   /// <summary>
@@ -281,42 +267,5 @@ internal sealed class BTreeLeafNode
     }
 
     throw new RecordNotFoundException("Record could not be found using the given primary key.");
-  }
-
-  /// <summary>
-  /// Performs a binary search on the leaf node page for the given search key.
-  /// </summary>
-  /// <param name="searchKey"></param>
-  /// <param name="deserializeKey"></param>
-  /// <returns>Positive integer value if exact match is found. Negative integer value representing
-  /// the insertion point index if no exact match is found.</returns>
-  private int FindSlotIndex(Key searchKey, Func<ReadOnlySpan<byte>, Key> deserializeKey)
-  {
-    // Get the item count from the page header...
-    var pageHeader = new PageHeader(_page);
-    var itemCount = pageHeader.ItemCount;
-
-    // Start our binary search through the records...
-    var low = 0;
-    var high = itemCount - 1;
-    while (low <= high)
-    {
-      var mid = low + (high - low) / 2;
-      var midPointKeyBytes = SlottedPage.GetRecord(_page, mid);
-      var midPointKey = deserializeKey(midPointKeyBytes);
-      var compareResult = _keyComparer.Compare(searchKey, midPointKey);
-
-      if (compareResult == 0)
-        return mid;
-      else if (compareResult < 0)
-        high = mid - 1;
-      else if (compareResult > 0)
-        low = mid + 1;
-    }
-
-    // If the loop completes, then we did not find an exact match, but the low
-    // value contains the insertion point. We return the bitwise complement
-    // of the insertion index.
-    return ~low;
   }
 }
---
+++ ArmDb.StorageEngine/BTreeNode.cs
@@ -0,0 +1,63 @@
+using ArmDb.DataModel;
+using ArmDb.SchemaDefinition;
+
+namespace ArmDb.StorageEngine;
+
+internal class BTreeNode
+{
+  protected readonly Page _page;
+  protected readonly TableDefinition _tableDefinition;
+  private readonly KeyComparer _keyComparer = new KeyComparer();
+
+  internal BTreeNode(Page page, TableDefinition tableDefinition)
+  {
+    ArgumentNullException.ThrowIfNull(page);
+    ArgumentNullException.ThrowIfNull(tableDefinition);
+
+    var header = SlottedPage.GetHeader(page);
+    if (header.PageType == PageType.Invalid)
+    {
+      throw new ArgumentException($"Received an invalid Page!", "page");
+    }
+
+    _page = page;
+    _tableDefinition = tableDefinition;
+  }
+
+  /// <summary>
+  /// Performs a binary search on the leaf node page for the given search key.
+  /// </summary>
+  /// <param name="searchKey"></param>
+  /// <param name="deserializeKey"></param>
+  /// <returns>Positive integer value if exact match is found. Negative integer value representing
+  /// the insertion point index if no exact match is found.</returns>
+  protected int FindSlotIndex(Key searchKey, Func<ReadOnlySpan<byte>, Key> deserializeKey)
+  {
+    // Get the item count from the page header...
+    var pageHeader = new PageHeader(_page);
+    var itemCount = pageHeader.ItemCount;
+
+    // Start our binary search through the records...
+    var low = 0;
+    var high = itemCount - 1;
+    while (low <= high)
+    {
+      var mid = low + (high - low) / 2;
+      var midPointKeyBytes = SlottedPage.GetRecord(_page, mid);
+      var midPointKey = deserializeKey(midPointKeyBytes);
+      var compareResult = _keyComparer.Compare(searchKey, midPointKey);
+
+      if (compareResult == 0)
+        return mid;
+      else if (compareResult < 0)
+        high = mid - 1;
+      else if (compareResult > 0)
+        low = mid + 1;
+    }
+
+    // If the loop completes, then we did not find an exact match, but the low
+    // value contains the insertion point. We return the bitwise complement
+    // of the insertion index.
+    return ~low;
+  }
+}
