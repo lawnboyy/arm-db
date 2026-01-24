57fa6038c27db5f35b90c9e166c0c324e7123272
Alan A. Hunt <a.a.hunt@gmail.com>
2026-01-24 13:35:08 -0600

    Implement methods for writing a query and a terminate message

--- ArmDb.Network/PacketWriter.cs
+++ ArmDb.Network/PacketWriter.cs
@@ -1,4 +1,4 @@
-using System.Buffers.Binary;
+using System.Text;
 using ArmDb.Common.Utils;
 
 namespace ArmDb.Network;
@@ -27,29 +27,81 @@ public class PacketWriter
     switch (packet.Type)
     {
       case PacketType.Connect:
-        var connectPacket = (ConnectPacket)packet;
-
-        // TODO: Consider using RecyclableMemoryStream package to reduce GC pressure here...
-        using (var payloadBuffer = new MemoryStream())
-        {
-          // Serialize the payload in Big Endian to write to the temporary payload buffer...
-          Span<byte> payloadSpan = stackalloc byte[4];
-          BinaryUtilities.WriteInt32BigEndian(payloadSpan, connectPacket.ProtocolVersion);
-          payloadBuffer.Write(payloadSpan);
-
-          // Write the header to the stream...
-          Span<byte> headerBuffer = stackalloc byte[5];
-          headerBuffer[0] = packetType;
-          BinaryUtilities.WriteInt32BigEndian(headerBuffer.Slice(1), (int)payloadBuffer.Length);
-          await _stream.WriteAsync(headerBuffer.ToArray(), 0, headerBuffer.Length, ct);
-
-          // Now write the payload to the stream...
-          payloadBuffer.Position = 0;
-          await _stream.WriteAsync(payloadBuffer.ToArray(), 0, (int)payloadBuffer.Length, ct);
-        }
+        ConnectPacket connectPacket = (ConnectPacket)packet;
+        await WriteConnectPacketAsync(connectPacket, ct);
         break;
+
+      case PacketType.Query:
+        QueryPacket queryPacket = (QueryPacket)packet;
+        await WriteQueryPacketAsync(queryPacket, ct);
+        break;
+
+      case PacketType.Terminate:
+        TerminatePacket terminatePacket = (TerminatePacket)packet;
+        await WriteTerminatePacketAsync(terminatePacket, ct);
+        break;
+
       default:
         throw new ArgumentException("Unknown packet type!");
     }
+  }
+
+  private async Task WriteConnectPacketAsync(ConnectPacket connectPacket, CancellationToken ct = default)
+  {
+    byte packetType = (byte)connectPacket.Type;
+
+    // TODO: Consider using RecyclableMemoryStream package to reduce GC pressure here...
+    using (var payloadBuffer = new MemoryStream())
+    {
+      // Serialize the payload in Big Endian to write to the temporary payload buffer...
+      Span<byte> payloadSpan = stackalloc byte[4];
+      BinaryUtilities.WriteInt32BigEndian(payloadSpan, connectPacket.ProtocolVersion);
+      payloadBuffer.Write(payloadSpan);
+
+      // Write the header to the stream...
+      Span<byte> headerBuffer = stackalloc byte[5];
+      headerBuffer[0] = packetType;
+      BinaryUtilities.WriteInt32BigEndian(headerBuffer.Slice(1), (int)payloadBuffer.Length);
+      await _stream.WriteAsync(headerBuffer.ToArray(), 0, headerBuffer.Length, ct);
+
+      // Now write the payload to the stream...
+      payloadBuffer.Position = 0;
+      await _stream.WriteAsync(payloadBuffer.ToArray(), 0, (int)payloadBuffer.Length, ct);
+    }
+  }
+
+  private async Task WriteQueryPacketAsync(QueryPacket queryPacket, CancellationToken ct = default)
+  {
+    byte packetType = (byte)queryPacket.Type;
+
+    // TODO: Consider using RecyclableMemoryStream package to reduce GC pressure here...
+    using (var payloadBuffer = new MemoryStream())
+    {
+      // Write the Sql string to the temporary buffer so we can determine the length; include null terminator at the end.
+      payloadBuffer.Write([.. Encoding.UTF8.GetBytes(queryPacket.Sql), 0]);
+
+      // Write the header to the stream...
+      Span<byte> headerBuffer = stackalloc byte[5];
+      headerBuffer[0] = packetType;
+      BinaryUtilities.WriteInt32BigEndian(headerBuffer.Slice(1), (int)payloadBuffer.Length);
+      await _stream.WriteAsync(headerBuffer.ToArray(), 0, headerBuffer.Length, ct);
+
+      // Now write the payload to the stream...
+      payloadBuffer.Position = 0;
+      await _stream.WriteAsync(payloadBuffer.ToArray(), 0, (int)payloadBuffer.Length, ct);
+    }
+  }
+
+  private async Task WriteTerminatePacketAsync(TerminatePacket terminatePacket, CancellationToken ct = default)
+  {
+    byte packetType = (byte)terminatePacket.Type;
+
+    // There is no payload for the terminate packet so we don't need to calculate the length.
+
+    // Write the header to the stream...
+    Span<byte> headerBuffer = stackalloc byte[5];
+    headerBuffer[0] = packetType;
+    BinaryUtilities.WriteInt32BigEndian(headerBuffer.Slice(1), 0);
+    await _stream.WriteAsync(headerBuffer.ToArray(), 0, headerBuffer.Length, ct);
   }
 }
